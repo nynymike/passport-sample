5ad7d08537418d9e85a3a12b30a1602542682aa6
Rafael Toledo <rafaelt91@gmail.com>
2017-05-01 13:09:15 -0300

    make it better

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ var app      = express();
 var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var session      = require('express-session');
+var session  = require('express-session');
 var flash    = require('connect-flash');
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/vast-refuge-14521';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -17,7 +28,16 @@ var bodyParser   = require('body-parser');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+// mongoose.connect(configDB.url); // connect to our database
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 require('./config/passport')(passport); // pass passport for configuration
 
