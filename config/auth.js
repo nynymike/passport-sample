6c4d4c96971e85a7c9fb78c8485ad4010490a41c
Rafael Toledo <rafaelt91@gmail.com>
2017-05-01 13:32:10 -0300

    make it better

--- config/auth.js
+++ config/auth.js
@@ -3,14 +3,14 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '114997209056735', // your App ID
         'clientSecret'  : '8614382600c17237849d4c05a895f432', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'https://vast-refuge-14521.herokuapp.com/auth/facebook/callback',
         profileFields   : ['name','emails']
     },
 
     'openidconnectAuth' : {
         'clientID'      : '@!EDC1.2B96.1235.CBC7!0001!099F.E817!0008!7486.21E5.9387.05C3', // your App ID
         'clientSecret'  : 'secret', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/openidconnect/callback',
+        'callbackURL'   : 'https://vast-refuge-14521.herokuapp.com/auth/openidconnect/callback',
         'issuer'          : 'https://pkg-deb11.gluu.org/',
         'authorizationURL' : 'https://pkg-deb11.gluu.org/oxauth/seam/resource/restv1/oxauth/authorize',
         'tokenURL' : 'https://pkg-deb11.gluu.org/oxauth/seam/resource/restv1/oxauth/token',
