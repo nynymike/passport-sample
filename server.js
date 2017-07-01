8aaa82928dabc76af4041c1dd2063f1a35ddf748
Rafael Toledo <rafaelt91@gmail.com>
2017-07-01 16:16:51 -0300

    Set port to be included in ipAddress

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var flash    = require('connect-flash');
 var bodyParser = require('body-parser');
 var request = require('request');
 var authParams = require('./config/auth');
+var ipAddress = require('./config/ip')(port);
 
 var fs = require('fs');
 var http = require('http');
@@ -83,7 +84,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport, request); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport, request, ipAddress); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 
