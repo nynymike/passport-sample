43c4e9c8d950ba104a564ae71b0f30d5c984b375
Rafael Toledo <rafaelt91@gmail.com>
2017-07-04 18:14:10 -0300

    Organize and clean code

--- app/routes.js
+++ app/routes.js
@@ -5,150 +5,152 @@ var OpenIdConnectStrategy = require('passport-openidconnect').Strategy;
 // load up the user model
 var User = require('../app/models/user');
 
-module.exports = function(app, passport, request, ipAddress) {
+module.exports = function (app, passport, request, ipAddress) {
 // normal routes ===============================================================
 
-	// show the home page (will also have our login links)
-	app.get('/proxy', function(req, res) {
-		res.render('index.ejs');
-	});
+    // show the home page (will also have our login links)
+    app.get('/proxy', function (req, res) {
+        res.render('index.ejs');
+    });
 
-	// authenticate the client with issuer info received
-	app.post('/proxy', function(req, res) {
-		request.get('http://' + ipAddress + '/auth/openidconnect?issuer=' + req.body.issuer, function (error, response, body) {
-			res.writeHead(response.statusCode, response.headers);
-			res.write(body);
-			res.end();
-		});
-	});
+    // authenticate the client with issuer info received
+    app.post('/proxy', function (req, res) {
+        request.get('http://' + ipAddress + '/auth/openidconnect?issuer=' + req.body.issuer, function (error, response, body) {
+            res.writeHead(response.statusCode, response.headers);
+            res.write(body);
+            res.end();
+        });
+    });
 
-	// PROFILE SECTION =========================
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user
-		});
-	});
+    // PROFILE SECTION =========================
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user
+        });
+    });
 
-	// LOGOUT ==============================
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/proxy');
-	});
+    // LOGOUT ==============================
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/proxy');
+    });
 
 // =============================================================================
 // AUTHENTICATE (FIRST LOGIN) ==================================================
 // =============================================================================
 
 
-	// openidconnect --------------------------------
+    // openidconnect --------------------------------
 
-		// send to openidconnect to do the authentication
-		// app.get('/auth/openidconnect', passport.authenticate('openidconnect', { scope : 'email' , response_types: 'code'}));
-		app.get('/auth/openidconnect', authenticate, function(req,res){});
+    // send to openidconnect to do the authentication
+    // app.get('/auth/openidconnect', passport.authenticate('openidconnect', { scope : 'email' , response_types: 'code'}));
+    app.get('/auth/openidconnect',
+        authenticate,
+        passport.authenticate('openidconnect', {scope: 'email', response_types: 'code'}),
+        function (req, res) {
 
-
-		function authenticate (req, res, next){
-
-			passport.use(new OpenIdConnectStrategy({
-																		// allows us to pass in the req from our route
-	        passReqToCallback : true, //(lets us check if a user is logged in or not)
-	        issuer          : req.query.issuer,
-
-	    } ,
+        });
 
 
-	    function(req, iss, sub, profile, jwtClaims, accessToken, refreshToken, params, done) {
-	      process.nextTick(function() {
-						// check if the user is already logged in
-	          if (!req.user) {
-						//
-	              // find the user in the database based on their openidconnect id
-	              User.findOne({ 'openidconnect.id' : profile.id }, function(err, user) {
+    function authenticate(req, res, next) {
 
-	                  // if there is an error, stop everything and return that
-	                  // ie an error connecting to the database
-	                  if (err)
-	                      return done(err);
+        passport.use(new OpenIdConnectStrategy({
+                // allows us to pass in the req from our route
+                passReqToCallback: true, //(lets us check if a user is logged in or not)
+                issuer: req.query.issuer,
 
-	                  // if the user is found, then log them in
-	                  if (user) {
-	                      return done(null, user); // user found, return that user
-	                  } else {
-	                      // if there is no user found with that openidconnect id, create them
-	                      var newUser            = new User();
+            },
 
-	                      // set all of the openidconnect information in our user model
-	                      newUser.openidconnect.id    = profile.id; // set the users openidconnect id
-	                      newUser.openidconnect.issuer = iss;
-												newUser.openidconnect.userName = profile._json.preferred_username;
-												newUser.openidconnect.displayName = profile.displayName;
-												newUser.openidconnect.idToken = params.id_token;
 
-	                      // save our user to the database
-	                      newUser.save(function(err) {
-	                          if (err)
-	                              throw err;
+            function (req, iss, sub, profile, jwtClaims, accessToken, refreshToken, params, done) {
+                // check if the user is already logged in
+                if (!req.user) {
+                    //
+                    // find the user in the database based on their openidconnect id
+                    User.findOne({'openidconnect.id': profile.id}, function (err, user) {
 
-	                          // if successful, return the new user
-														return done(null, newUser);
-	                      });
-	                  }
-	              });
-	          } else {
-	              // user already exists and is logged in, we have to link accounts
-	              var user            = req.user; // pull the user out of the session
-	              // update the current users openidconnect credentials
-	              user.openidconnect.id    = profile.id; // set the users openidconnect id
-	              user.openidconnect.issuer = iss;
-	              user.openidconnect.userName = profile._json.preferred_username;
-								user.openidconnect.displayName = profile.displayName;
-								user.openidconnect.idToken = params.id_token;
+                        // if there is an error, stop everything and return that
+                        // ie an error connecting to the database
+                        if (err)
+                            return done(err);
 
-	              // save the user
-	              user.save(function(err) {
-	                  if (err)
-	                      throw err;
-												return done(null, user);
-	            	});
-	        	}
-	    	});
-			}));
+                        // if the user is found, then log them in
+                        if (user) {
+                            return done(null, user); // user found, return that user
+                        } else {
+                            // if there is no user found with that openidconnect id, create them
+                            var newUser = new User();
 
-			passport.authenticate('openidconnect', { scope : 'email' , response_types: 'code'})(req, res, next)
-		}
+                            // set all of the openidconnect information in our user model
+                            newUser.openidconnect.id = profile.id; // set the users openidconnect id
+                            newUser.openidconnect.issuer = iss;
+                            newUser.openidconnect.userName = profile._json.preferred_username;
+                            newUser.openidconnect.displayName = profile.displayName;
+                            newUser.openidconnect.idToken = params.id_token;
 
-		// handle the callback after openidconnect has authenticated the user
-		app.get('/auth/openidconnect/callback', passport.authenticate('openidconnect', {
-				successRedirect : '/profile',
-				failureRedirect : '/proxy'
-			}));
-		// }), callbackResponse);
+                            // save our user to the database
+                            newUser.save(function (err) {
+                                if (err)
+                                    throw err;
+
+                                // if successful, return the new user
+                                return done(null, newUser);
+                            });
+                        }
+                    });
+                } else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user = req.user; // pull the user out of the session
+                    // update the current users openidconnect credentials
+                    user.openidconnect.id = profile.id; // set the users openidconnect id
+                    user.openidconnect.issuer = iss;
+                    user.openidconnect.userName = profile._json.preferred_username;
+                    user.openidconnect.displayName = profile.displayName;
+                    user.openidconnect.idToken = params.id_token;
+
+                    // save the user
+                    user.save(function (err) {
+                        if (err)
+                            throw err;
+                        return done(null, user);
+                    });
+                }
+            }));
+        next();
+    }
+
+    // handle the callback after openidconnect has authenticated the user
+    app.get('/auth/openidconnect/callback', passport.authenticate('openidconnect', {
+        successRedirect: '/profile',
+        failureRedirect: '/proxy'
+    }));
+    // }), callbackResponse);
 
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
-	return next();
+    if (req.isAuthenticated())
+        return next();
 
-	res.redirect('/proxy');
+    res.redirect('/proxy');
 }
 
 
 function callbackResponse(req, res) {
-	if (!req.user) {
-		return res.redirect('/proxy');
-	}
-	console.log('User authenticated with: ' + req.user.openidconnect.issuer + ' Strategy with userid: ' + req.user.openidconnect.id);
-	var queryUserString = encodeURIComponent(JSON.stringify(req.user.openidconnect));
-	var options = {
-		url: req.authInfo.state.userInfoURL,
-		headers: {
-			'Authorization': 'Bearer ' + req.user.openidconnect.accessToken
-		}
-	};
-	request.post(options, function (error, response, body) {
-		var userInfo = JSON.parse(body);
-		console.log(userInfo);
-	});
+    if (!req.user) {
+        return res.redirect('/proxy');
+    }
+    console.log('User authenticated with: ' + req.user.openidconnect.issuer + ' Strategy with userid: ' + req.user.openidconnect.id);
+    var queryUserString = encodeURIComponent(JSON.stringify(req.user.openidconnect));
+    var options = {
+        url: req.authInfo.state.userInfoURL,
+        headers: {
+            'Authorization': 'Bearer ' + req.user.openidconnect.accessToken
+        }
+    };
+    request.post(options, function (error, response, body) {
+        var userInfo = JSON.parse(body);
+        console.log(userInfo);
+    });
 };
